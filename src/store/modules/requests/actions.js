2910c1c16ed65c884e9a6c55dd5f21dbecc340bb
Andrey2109 <andrey@podiumcomp.com>
2024-12-26 08:47:04 +0200

    Feature: create intermidiate requests module

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,8 +1,8 @@
 export default {
   contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString,
-      coachId: payload.id,
+      id: new Date().toISOString(),
+      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
