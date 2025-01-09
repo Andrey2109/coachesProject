b3bb5a212623c15e49f7fedb33c5dee1e7ced9d5
Andrey2109 <andrey@podiumcomp.com>
2025-01-09 19:47:15 +0200

    feature: Adding a Login Action & Flow

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -74,14 +74,15 @@ export default {
       }
 
       this.isLoading = true;
+      const actionPayload = {
+        email: this.email,
+        password: this.password,
+      };
       try {
         if (this.mode === 'login') {
-          // Login functionality will be implemented later
+          await this.$store.dispatch('login', actionPayload);
         } else {
-          await this.$store.dispatch('signup', {
-            email: this.email,
-            password: this.password,
-          });
+          await this.$store.dispatch('signup', actionPayload);
         }
       } catch (err) {
         this.error = err.message || 'Failed to authenticate, try later.';
